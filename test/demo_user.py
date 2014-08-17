a99d79dad057b625a57a592f327406f10151780a
miltonm <mkjee2001+github@gmail.com>
2014-08-17 09:19:21 +0100

    Logging and fixed range
    Added logging to demo_user. Also fixed a bug in demo_user to include
    last day in the sequence.

--- test/demo_user.py
+++ test/demo_user.py
@@ -9,7 +9,11 @@ cleaned.
 '''
 from __future__ import print_function
 import sys
-sys.path.append('..')
+import os
+import itertools
+# add our module dependencies to the path
+deps_path = os.path.realpath(os.path.dirname(__file__) + "/..")
+sys.path.append(deps_path)
 import argparse
 import os
 import tempfile
@@ -20,11 +24,17 @@ from worktracker import record_past
 from worktracker.libworktracker import record_db
 from worktracker.libworktracker import config
 from worktracker.libworktracker import date_time_utils
+from worktracker.libworktracker import io
 
 
 class DemoDb(object):
     def __init__(self):
-        pass
+        self.io = None
+        self.log = print
+
+    def set_io(self, io_inst):
+        self.io = io_inst
+        self.log = self.io.log
 
     def cleanse(self, args):
         shutil.rmtree(os.path.dirname(args.config_path))
@@ -39,23 +49,26 @@ class DemoDb(object):
                 'min_work_block': 1,
                 'timezone': 'Europe/London',
                 }
-        c1 = config.Config(overriding_params=o_params)
+        c1 = config.Config(overriding_params=o_params, logging_fn=self.log)
         return c1
 
     def last_n_eods(self, n, tz):
         (start_of_today, _ ) = date_time_utils.parse_date_to_ts_range(
                 "today", time.time(), tz)
+        print("start of today: ", start_of_today)
         eoy = start_of_today - 1
         day_in_secs = 24*60*60
-        return xrange(eoy - n*day_in_secs + 1, eoy, day_in_secs)
+        # add end of yesterday otherwise yesterday would be fully missed
+        return itertools.chain(xrange(eoy - n*day_in_secs + 1,
+            eoy, day_in_secs), [eoy])
 
     def sample_ts_dur(self, eod, x):
         day_in_secs = 24*60*60
         # get 10 sample time stamps in the day at least 30 mins apart
         samples = random.sample(range(eod - day_in_secs + 1, eod, 30*60), x)
         samples = sorted(samples)
-        # create tuples of sample timestamps and duration bet. the sample and the
-        # next one
+        # create tuples of sample timestamps and duration bet. the sample and
+        # the next one
         for i in xrange(x - 1):
             yield (samples[i], samples[i+1] - samples[i])
 
@@ -75,7 +88,8 @@ class DemoDb(object):
                 if args.config_path
                 else self.create_config())
         print(conf.config_file_path)
-        db_table = record_db.create_record_table(conf.db_full_path, print)
+        db_table = record_db.create_record_table(conf.db_full_path,
+                self.log)
         # prepopulate data for last 5 weeks
         for eod in self.last_n_eods(35, conf.timezone):
             for (ts, diff) in self.sample_ts_dur(eod, 10):
@@ -101,4 +115,6 @@ if __name__=='__main__':
     parser_cleanse.add_argument("config_path")
     parser_cleanse.set_defaults(func=demo.cleanse)
     args = parser.parse_args()
+    io_inst = io.InOut(print, log_dir=args.log_dir)
+    demo.set_io(io_inst)
     args.func(args)
